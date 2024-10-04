e9ba2be5565c2f25c15b8f253e9e80140b95515e
Akshay <147636352+AkshayKumar-58@users.noreply.github.com>
2024-10-04 21:57:45 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+// There are 8 types of datatypes(7 primitive datatypes and 1 non primitive datatypes)
+// Number => used to store integer and float values upto 2 ki power 53
+// BigInt => used for bigger integer values
+// Boolean => used for True or false values
+// String  => storing characters and sentences
+// null => for showing null values stand alone value
+// Symbol =>to find uniqueness in components specially in react
+// Undefined => no value is assigned
+
+// and non primitive]
+// Object
+
+//for tabular show use table in console
+let name ="javascript";
+let age=34;
+let city="jaipur";
+let state="Rajasthan";
+console.table([name,age,city,state]);
+console.table([typeof name, typeof age, typeof city, typeof state]);
+console.log(typeof null); //it is an object
+console.log(typeof undefined); 
