ac4a7d5284b460f303b2301782cb795d8fdff28d
Akshay <147636352+AkshayKumar-58@users.noreply.github.com>
2024-10-04 22:01:40 +0530

    comparison operators and value types

---
+++ comparison.js
@@ -0,0 +1,19 @@
+// console.log( 2 >= 1);
+// and so on so 
+
+console.log("2" > 1);
+console.log("02" > 1);
+//note always compare between same datatypes
+//due to differente results we avoid it
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);// this comparison operator converts null to NUmber
+//some operators converts into NUMber and some converts in NaN(Not a Number)
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+//strict check === this checks value and also the datatype of the variable
+console.log("2" === 2);
