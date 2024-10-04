6ac15dcb26faefbef0c206addf94090528f854e9
Akshay <147636352+AkshayKumar-58@users.noreply.github.com>
2024-10-04 21:59:29 +0530

    conversion of datatypes

---
+++ conversion.js
@@ -0,0 +1,75 @@
+let score =33; //this is a number
+
+//converting into string
+let stringScore = String(score);
+
+console.log("typeof score is :" + typeof score);
+
+console.log("typeof stringScore is :" + typeof stringScore);
+
+
+//coverting string to number
+let Name= "Akshay";
+
+let numberName= Number(Name);
+
+console.log("type of Name : " + typeof Name);
+
+console.log("type of numberName : " + typeof numberName);
+
+//null value
+let temperature=null;
+
+let tempNUllchange= Number(temperature);
+
+console.log("type of temperature : " + typeof temperature);
+
+console.log("change of null in number :" + tempNUllchange);
+
+console.log("type of tempNULL : " + typeof tempNUllchange);
+
+//boolean converting
+let rain=true;
+
+let notrain=false;
+
+let changeRain=Number(rain);
+
+console.log("value of changing rain in number :" + changeRain);
+
+console.log("type of rain : " + typeof changeRain);
+
+console.log(rain);
+
+console.log(notrain);
+
+let changeNotrain = Number(notrain);
+
+console.log("type of changeNotrain : " + typeof changeNotrain);
+
+let a=0;
+
+let b=1;
+
+let changeA = Boolean(a);
+
+console.log("type of a : " + typeof a);
+
+console.log("type of changeA : " + typeof changeA);
+
+let changeB = Boolean(b);
+
+console.log("type of bL : " + typeof b);
+
+console.log("type of changeB : " + typeof changeB);
+
+let str1 = "";
+let str2 = "ghlgu";
+
+let changestr1 = Number(str1);
+console.log(changestr1);
+
+let changestr2 = Number(str2);
+console.log(changestr2);
+
+ 
