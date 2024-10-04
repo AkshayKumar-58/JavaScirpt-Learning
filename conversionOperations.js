201ca28d8027f0b9f3c42c98c64b841c420aca33
Akshay <147636352+AkshayKumar-58@users.noreply.github.com>
2024-10-04 22:00:15 +0530

    Conversion operations and precedences of operators

---
+++ conversionOperations.js
@@ -0,0 +1,22 @@
+// let value=3;
+// let negValue = -value;
+// console.log(negValue);
+// //operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //power operator
+// console.log(2/3);
+// console.log(2%3); //remainder operator
+
+let str1 = "hello";
+let str2 = " akshay";
+//to add string
+
+let str3 = str1 + str2;
+console.log(str3);
+
+//if first operand is string then rest is consider as string
+console.log("Answer would be 22 " +"2" + 2);
+//if firs operand are numbers and then string comes in smae order
+console.log(3 + 4 + "634" + " Answer would be 7634");
